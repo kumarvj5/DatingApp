abbeb5921c35ba8b23231e64d8ce8671f248f8d6
vijay kumar <you@example.com>
2019-06-07 22:46:43 -0700

    Initial commit

---
+++ KaamDatingApp.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using KaamDatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KaamDatingApp.API.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options){}
+        public DbSet<Value> Values { get; set; }
+    }
+}
