5de5fe88ba118aeed17a067a02d63c194955f193
vijay kumar <you@example.com>
2019-06-14 22:23:59 -0700

    Adding 3rd party components

--- KaamDatingApp-SPA/src/app/_services/auth.service.ts
+++ KaamDatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 baseurl = 'http://localhost:5000/api/auth/';
+jwtHelper = new JwtHelperService();
+decodedToken: any;
 
 constructor(private http: HttpClient) { }
+
 login(model: any) {
   return this.http.post(this.baseurl + 'login', model)
   .pipe(
@@ -16,6 +20,7 @@ login(model: any) {
       const user = response;
       if (user) {
         localStorage.setItem('token', user.token);
+        this.decodedToken = this.jwtHelper.decodeToken(user.token);
       }
     })
   )
@@ -23,5 +28,10 @@ login(model: any) {
 
 register(model: any){
   return this.http.post(this.baseurl + 'register', model);
+}
+
+loggedIn(){
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
 }
 }
